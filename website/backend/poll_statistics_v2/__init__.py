6e40fc7863e85502bf15daa842bb8aa76b7edf9b
Robert <61035696+robertborowski@users.noreply.github.com>
2023-07-29 09:41:32 -0400

    alright

--- website/backend/poll_statistics_v2/__init__.py
+++ website/backend/poll_statistics_v2/__init__.py
@@ -22,30 +22,42 @@ def arr_to_shortened_dict_function(page_dict, stat_name):
 # ------------------------ individual function end ------------------------
 
 # ------------------------ individual function start ------------------------
+def create_fake_count_stats_function(page_dict, stat_name, choices_arr, col_name):
+  return page_dict
+# ------------------------ individual function end ------------------------
+
+# ------------------------ individual function start ------------------------
 def get_count_and_percent_stats_function(page_dict, stat_name, choices_arr, col_name):
   objs_arr_of_dicts = page_dict['poll_statistics_v2_dict'][stat_name]['all_response_objs']
   total_denominator = len(objs_arr_of_dicts)
-  # ------------------------ loop through submitted answers to get answer choice distribution start ------------------------
-  # set count and percent to 0
+  # ------------------------ set count to 0 for all options start ------------------------
   page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict'] = {}
   page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['count_dict'] = {}
   page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['percent_dict'] = {}
   for i in choices_arr:
     page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['count_dict'][i] = 0
     page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['percent_dict'][i] = 0
-  # loop + update count
+  # ------------------------ set count to 0 for all options end ------------------------
+  # ------------------------ get real count start ------------------------
   for i_dict in objs_arr_of_dicts:
     try:
       page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['count_dict'][i_dict[col_name]] += 1
     except:
       pass
-  # loop + update percents
+  # ------------------------ get real count end ------------------------
+  # ------------------------ get fake count if needed start ------------------------
+  # min_votes_limit = 100
+  # page_dict['poll_statistics_v2_dict']['min_votes_limit'] = min_votes_limit
+  # if total_denominator < min_votes_limit:
+  #   page_dict = create_fake_count_stats_function(page_dict, stat_name, choices_arr, col_name)
+  # ------------------------ get fake count if needed end ------------------------
+  # ------------------------ get percents start ------------------------
   for k,v in page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['count_dict'].items():
     try:
       page_dict['poll_statistics_v2_dict'][stat_name][col_name+'_dict']['percent_dict'][k] = str(float(float(v) / float(total_denominator)) * float(100))+'%'
     except:
       pass
-  # ------------------------ loop through submitted answers to get answer choice distribution end ------------------------
+  # ------------------------ get percents end ------------------------
   return page_dict
 # ------------------------ individual function end ------------------------
 
