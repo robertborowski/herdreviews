8a2671a2eefa264775cbd4cc3762f44108245df1
Robert <61035696+robertborowski@users.noreply.github.com>
2023-08-01 07:42:08 -0400

    small

--- website/backend/dict_manipulation/__init__.py
+++ website/backend/dict_manipulation/__init__.py
@@ -38,12 +38,23 @@ def get_answers_shortened_v2_function(input_arr):
 # ------------------------ individual function end ------------------------
 
 # ------------------------ individual function start ------------------------
+def get_answers_lettered_function(input_arr):
+  alphabet_arr = get_alphabet_arr_function()
+  final_dict = {}
+  for i in range(len(input_arr)):
+    letter = alphabet_arr[i]
+    final_dict[letter] = input_arr[i]
+  return final_dict
+# ------------------------ individual function end ------------------------
+
+# ------------------------ individual function start ------------------------
 def prep_poll_dict_function(poll_dict):
   # ------------------------ separate answer choices start ------------------------
   poll_dict['answer_choices'] = poll_dict['answer_choices'].split('~')
   # ------------------------ separate answer choices end ------------------------
   # ------------------------ dict for answer + letter association start ------------------------
   poll_dict['answer_choices_dict'] = get_answers_shortened_v2_function(poll_dict['answer_choices'])
+  poll_dict['answer_choices_dict_letters_only'] = get_answers_lettered_function(poll_dict['answer_choices'])
   # ------------------------ dict for answer + letter association end ------------------------
   return poll_dict
 # ------------------------ individual function end ------------------------
